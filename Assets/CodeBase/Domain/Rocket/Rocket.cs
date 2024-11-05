f1a9b25ecf9f13b446d1268338c597ea769bedff
CallMeNAKS <thefan51999@gmail.com>
2024-11-05 16:44:42 +0300

    change rocket collision logic

--- Assets/CodeBase/Domain/Rocket/Rocket.cs
+++ Assets/CodeBase/Domain/Rocket/Rocket.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using CodeBase.Domain.Enemy;
 using UnityEngine;
 
 namespace Domain.Rocket
@@ -7,19 +7,34 @@ namespace Domain.Rocket
     public class Rocket : MonoBehaviour
     {
         private Rigidbody _rigidbody;
-        
+
         [SerializeField] private float _speed;
         [SerializeField] private float _damage;
-        
-        public float Damage { get { return _damage; } private set { _damage = value; } }
+
+        public float Damage
+        {
+            get { return _damage; }
+            private set { _damage = value; }
+        }
+
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
         }
-        
+
         public void ApplyPower(Vector3 force)
         {
             _rigidbody.AddForce(force, ForceMode.VelocityChange);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            var enemy = other.gameObject.GetComponentInParent<AbstractEnemy>();
+
+            if (enemy)
+            {
+                enemy.TakeDamage(_damage);
+            }
         }
     }
 }
