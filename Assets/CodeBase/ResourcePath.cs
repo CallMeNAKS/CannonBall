c84ac8229057e0adaf67fafe9a2a48ce6d577929
CallMeNAKS <thefan51999@gmail.com>
2024-11-26 21:34:27 +0300

    del legacy files

--- Assets/CodeBase/ResourcePath.cs
+++
@@ -1,5 +0,0 @@
-﻿public static class ResourcePath
-{
-    public static readonly string CannonBall = "Ball/Cannon Ball";
-    public static readonly string Level1Target = "Projectile/Level1Projectile";
-}
